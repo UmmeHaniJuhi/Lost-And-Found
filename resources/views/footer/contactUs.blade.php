a490b042d277ed41a2f0300094d1165646555a1b
yeard-morshed <yeardmorshed1999@gmail.com>
2022-08-02 23:05:21 +0600

    contact

--- resources/views/footer/contactUs.blade.php
+++ resources/views/footer/contactUs.blade.php
@@ -1,1 +1,123 @@
-<h1>Contact Us</h1>
+<!DOCTYPE html>
+<html>
+<head>
+<!--Boostrap 5 CDN linl --> <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!--Custom file link -->
+ <!--<link rel="stylesheet" href="styleee.css"> -->>
+ <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
+
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+<body  data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="75">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        <a class="navbar-brand ml-5" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            </ul>
+
+            <div class="d-flex navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Urgent</a>
+                </li>
+                &nbsp;
+                &nbsp;&nbsp;
+                &nbsp;
+                
+                &nbsp;
+                &nbsp;
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+
+    <section id="contact" class="contact_wrapper">
+        <div class="container">
+          <div class="row  justify-content-between align-items-center ">
+            <div class="col-lg-6 order-2 order-lg-1 pe-lg-5 text-lg-start text-center">
+              <span class="subtitle">As-salamu alaykum </span>
+                  <h2>Contact Us</h2>
+    
+                  <div class="row call_details mb-4">
+                    <label class="col-sm-3 col-lg-4 "> Call us directly:</label>
+                    <div class="col-sm-9 col-lg-8 mb-3 mb-lg-2 text=mb-start">
+                      <a href="javascript:void(0)">+8801711000000</a>
+                    </div>
+                  </div>
+    
+                  <div class="row call_details mb-4">
+                    <label class="col-sm-3 col-lg-4 "> Contact Email:</label>
+                    <div class="col-sm-9 col-lg-8 mb-3 mb-lg-2 text=mb-start">
+                      <a href="lostandfound@gmail.com">lostandfound@gmail.com</a>
+                    </div>
+                  </div>
+    
+                  <form id="form">
+    
+                    <div class="mb-4">
+                      <input type="text" class="form-control" placeholder="Your Name.." autocomplete="off">
+                    </div>
+    
+                    <div class="mb-4">
+                      <input type="number" class="form-control" placeholder="Enter Phone Number.." autocomplete="off">
+                    </div>
+    
+                    <div class="mb-4">
+                      <input type="email" class="form-control" placeholder="Your Email.." autocomplete="off">
+                    </div>
+    
+                    <div class="mb-4">
+                      <textarea cols="40" rows="4" class="form-control" id="message" placeholder="Write a message..." autocomplete="off">
+    
+                      </textarea>
+                    </div>
+                    <button type="submit" class="main-btn">Submit</button> 
+    
+                  </form>
+    
+    
+            </div>
+    
+    
+            <div class="col-lg-6 order-1 mb-4 order-lg-1 mb-lg-0">
+              <img src="https://as2.ftcdn.net/v2/jpg/01/37/52/13/1000_F_137521329_Wf2xCXOsL8haVaZLiPx10T5e91HdIcke.jpg" class="img-fluid">
+              
+            </div>
+    
+          </div>
+        </div>
+       </section> 
+ 
+ 
+
+
+<!--Boostrap 5 CDN linl --> 
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+
+</body>
+</html>
