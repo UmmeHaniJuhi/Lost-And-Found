18e4f82f51770d09c350d785e586fac249198ccb
AzwadBhuiyan <azwadbhuiyan23@gmail.com>
2022-08-02 19:13:06 +0600

    checking webhook

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -144,7 +144,7 @@
             window.requestAnimationFrame(step);
         }
 
-        animatePostsCounter(document.getElementById("count"), 0, {{json_encode(50)}}, 1200);
+        animatePostsCounter(document.getElementById("count"), 0, {{json_encode(56)}}, 1200);
     </script>
 
 
